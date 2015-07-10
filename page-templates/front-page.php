4a653dc68839b1a1952213b64e0edb51a9cac9e9
ElDav1d <dvivoes@yahoo.es>
2015-07-10 12:33:48 +0200

    Adding content to front-page

--- page-templates/front-page.php
+++ page-templates/front-page.php
@@ -8,6 +8,328 @@
 
 get_header(); ?>
 
+		<section id="WTF" class="row">
+			<div class="large-12 medium-12 small-12 columns total-align">
+				<h2>DEVSCOLA WTF?</h2>
+				<h3>Jarldenawer</h3>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. 
+				</p>
+			</div>
+		</section>
+
+		<section id="TRACKS" class="row">
+			<div class="large-12 medium-12 small-12 columns total-align">
+				<h2>TRACKS</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis  
+				</p>
+			</div>
+			
+			<div class="large-12 columns total-align">
+				<div class="large-4 medium-4 small-12 columns left_column"> 
+					<img class="logo-track" src="assets/images/LogoFrontend.svg" alt="Logos HTML5 CSS3 y JS">
+					<h4>Frontend</h4>
+					<p>Partimos de las bases de la creación de páginas web con HTML5 y CSS3 para luego aprender a introducir dinamismo con Javascript. Aprenderemos jQuery y cuando estemos familiarizados con el lenguaje comenzaremos a profundizar en uno de los frameworks más populares: Angular. 
+					</p>
+				</div>
+
+				<div class="large-4 medium-4 small-12 columns center_column"> 
+					<img class="large-12 logo-track" src="assets/images/LogoRuby.svg" alt="Logos Ruby  y Ruby on Rails">
+					<h4>Ruby</h4>
+					<p>Aprenderemos las bases del lenguaje Ruby para luego explorar la creación de aplicaciones web usando sus frameworks más populares: Sinatra para aprender las bases y Ruby on Rails con el que avanzaremos a completas aplicaciones y aprenderemos las últimas arquitecturas de referencia.
+					</p>
+				</div>
+
+				<div class="large-4 medium-4 small-12 columns right_column"> 
+					<img class="logo-track" src="assets/images/LogoJava.svg" alt="Logo Java">
+					<h4>Java</h4>
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa.
+					</p>
+				</div>
+			</div>	
+		</section>
+
+		<section id="COMPROMISO" class="row">
+			<div class="large-12 medium-12 small-12 columns total-align">
+				<h2 >COMPROMISO</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. 
+				</p>
+			</div>
+		</section>
+
+		<section id="WHO" class="row">
+
+			<div class="large-12 medium-12 small-12 columns total-align">
+				<h2>DEVSCOLA WHO?</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. 
+				</p>
+			</div>
+		</section>
+
+	
+		<section id="WHO-PICS" class="row">
+			<h2>INTEGRANTES</h2>
+			<div class="large-12 medium-12 small-12 columns total-align" >
+				<div class="large-2 medium-2 small-4 columns left_column">
+					<img src="assets/images/Who1.jpg" alt="Who1">
+				</div>
+
+				<div class="large-4 medium-4 small-8 columns center_column">
+					<h3>Jarldenawer</h3>
+					<h5>MENTOR</h5>	
+					<p>Ricardo es Agile Software Developer, profesor en los cursos de Desarrollo Web Avanzado, HTML5/CSS/JavaScript y Desarrollo Ágil, creador de CryptoApplet y organiza decharlas.com</p>
+				</div>
+
+				<div class="large-2 medium-2 small-4 columns center_column">
+					<img src="assets/images/Who2.jpg" alt="Who1">
+				</div>
+
+				<div class="large-4 medium-4 small-8 left columns right_column">
+					<h3>Jarldenawer</h3>
+					<h5>ALUMNI 2014</h5>	
+					<p>Ricardo es Agile Software Developer, profesor en los cursos de Desarrollo Web Avanzado, HTML5/CSS/JavaScript y Desarrollo Ágil, creador de CryptoApplet y organiza decharlas.com</p>
+				</div>
+			</div>
+
+			<div class="large-12 medium-12 small-12 columns total-align">
+
+				<div class="large-2 medium-2 small-4 columns left_column">
+					<img src="assets/images/Who3.jpg" alt="Who1">
+				</div>
+
+				<div class="large-4 medium-4 small-8 columns center_column">
+					<h3>Jarldenawer</h3>
+					<h5>MENTOR</h5>
+					<p>Ricardo es Agile Software Developer, profesor en los cursos de Desarrollo Web Avanzado, HTML5/CSS/JavaScript y Desarrollo Ágil, creador de CryptoApplet y organiza decharlas.com</p>
+				</div>
+
+				<div class="large-2 medium-2 small-4 columns medium-2 center_column">
+					<img src="assets/images/Who4.jpg" alt="Who1">
+				</div>
+
+				<div class="large-4 medium-4 small-8 left columns right_column">
+					<h3>Jarldenawer</h3>
+					<h5>ALUMNI 2014</h5>
+					<p>Xavi es el alma de la escuela. Es un programador con prestigio a nivel nacional. Es usual verlo encima de los escenarios dando charlas en conferencias técnicas en España</p>
+				</div>
+			</div>
+
+			<div class="large-12 medium-12 small-12 columns total-align">
+				<div class="large-2 medium-2 small-4 columns left_column">
+					<img src="assets/images/Who5.jpg" alt="Who1">
+				</div>
+
+				<div class="large-4 medium-4 small-8 columns center_column">
+					<h3>Jarldenawer</h3>
+					<h5>MENTOR</h5>
+					<p>Miguel Angel es un programador generalista con experiencia en universidades,  consultoras, startups y como autónomo. Ha programado en múltiples lenguajes, en backend y frontend.</p>
+				</div>
+
+				<div class="large-2 medium-2 small-4 columns center_column">
+					<img src="assets/images/Who6.jpg" alt="Who1">
+				</div>
+
+				<div class="large-4 medium-4 small-8 left columns right_column">
+					<h3>Jarldenawer</h3>
+					<h5>ALUMNI 2014</h5>
+					<p>Ricardo es Agile Software Developer, profesor en los cursos de Desarrollo Web Avanzado, HTML5/CSS/JavaScript y Desarrollo Ágil, creador de CryptoApplet y organiza decharlas.com</p>
+				</div>
+			</div>
+
+			<div class="large-12 medium-12 small-12 columns total-align">
+
+				<div class="large-2 medium-2 small-4 columns left_column">
+					<img src="assets/images/Who7.jpg" alt="Who1">
+				</div>
+
+				<div class="large-4 medium-4 small-8 columns center_column">
+					<h3>Jarldenawer</h3>
+					<h5>MENTOR</h5>
+					<p>Ricardo es Agile Software Developer, profesor en los cursos de Desarrollo Web Avanzado, HTML5/CSS/JavaScript y Desarrollo Ágil, creador de CryptoApplet y organiza decharlas.com</p>
+				</div>
+
+				<div class="large-2 medium-2 small-4 columns center_column">
+					<img src="assets/images/Who8.jpg" alt="Who1">
+				</div>
+
+				<div class="large-4 medium-4 small-8 left columns right_column">
+					<h3>Jarldenawer</h3>
+					<h5>ALUMNI 2014</h5>
+					<p>Ricardo es Agile Software Developer, profesor en los cursos de Desarrollo Web Avanzado, HTML5/CSS/JavaScript y Desarrollo Ágil, creador de CryptoApplet y organiza decharlas.com</p>
+				</div>
+
+			</div>
+
+		</section>
+				
+			
+		
+		<section id="MENTORING" class="row">
+			<div class="large-12 medium-12 small-12 columns total-align">
+				<h2>MENTORING</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. 
+				</p>
+			</div>
+
+			<!--owl carousel-->
+
+			<div class="large-12 medium-12 small-12 columns total-align">
+				<div id="owl-demo" class="owl-carousel">
+
+				  <div><img src="assets/images/B8_93neIMAA779n.jpg" alt="Devscola en Geekshubs"></div>
+				  <div><img src="assets/images/B9lKLjjIYAAm67Q.jpg" alt="Devscola en Geekshubs"></div>
+				  <div><img src="assets/images/B9lNVh8IAAAWCOt.jpg" alt="Devscola en Geekshubs"></div>
+				  <div><img src="assets/images/B9lNx1CIAAE18Qp.jpg" alt="Devscola en Geekshubs"></div>
+				  <div><img src="assets/images/B-SJ5AUIIAAKMZ3.jpg" alt="Devscola en Geekshubs"></div>
+
+				</div>
+			</div>
+		</section>
+
+
+
+		<section id="JOIN" class="row">
+
+			<div class="large-12 medium-12 small-12 columns total-align">
+				<h2>PARTICIPAR</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. 
+				</p>
+				<div class="large-12 medium-12 small-12 columns align-right">
+					<a class="large button right" href="mailto:hola@devscola.org">Quiero formar parte de ésta comunidad</a>
+				</div>
+			</div>
+
+		</section>
+
+		<section id="SPONSOR" class="row">
+
+			<div class="large-12 medium-12 small-12 columns total-align">
+				<h2>APOYAR</h2>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. 
+				</p>
+				<div class="large-12 medium-12 small-12 columns align-right">
+					<a class="large button right" href="mailto:hola@devscola.org">Quiero formar parte de vuestro proyecto</a>
+				</div>
+			</div>
+
+		</section>
+
+		<section id="SOCIAL" class="row">
+
+			<div class="large-12 medium-12 small-12 columns total-align">
+
+			<h2>SOCIAL</h2>
+
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+				<br>Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
+ 				</p>
+
+			</div>
+			<div class="large-12 medium-12 small-12 columns total-align">
+
+				<div class="large-4 medium-4 small-4 columns left_column">
+					<a class="large button social_button" href="https://www.youtube.com/channel/UCfFeLBv3iYU8ZnVswlO022A">
+					<div class="large-3 medium-3 columns left_column">
+					<img class="in_button" src="assets/images/Logotipo-06.png" alt="Logo Devscola">
+					</div>Visítanos en Youtube</a>
+				</div>
+
+				<div class="large-4 medium-4 small-4 columns center_column">
+					<a class="large button social_button" href="https://twitter.com/devscola">
+					<div class="large-3 medium-3 columns left_column">
+					<img class="in_button" src="assets/images/Logotipo-06.png" alt="Logo Devscola">
+					</div>Síguenos en Twitter</a>
+				</div>
+				
+				<div class="large-4 medium-4 small-4 columns right_column">
+					<a class="large button social_button" href="http://devscola.org/blog">
+					<div class="large-3 medium-3 columns left_column">
+					<img class="in_button" src="assets/images/Logotipo-06.png" alt="Logo Devscola">
+					</div>Sigue nuestro blog</a>
+				</div>
+				
+			</div>
+			
+			<div class="large-12 medium-12 small-12 columns total-align">
+
+				<h4>MASTERCLASSES</h4>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+				</p>
+
+			</div>
+
+			<div class="large-12 medium-12 small-12 columns total-align">
+
+				<div class="large-4 medium-4 small-12 columns left_column">
+					<div class="flex-video">
+					<iframe  src="https://www.youtube.com/embed/m89tB8n-RQg" width="400" height="225" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+
+				<div class="large-4 medium-4 small-12 columns center_column">
+					<div class="flex-video">
+					<iframe  src="https://www.youtube.com/embed/m89tB8n-RQg" width="400" height="225" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+
+				<div class="large-4 medium-4 small-12 columns right_column">
+					<div class="flex-video">
+					<iframe  src="https://www.youtube.com/embed/m89tB8n-RQg" width="400" height="225" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="large-12 medium-12 small-12 columns total-align">
+
+				<h4>ASAMBLEAS</h4>
+
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+				</p>
+
+			</div>
+
+			<div class="large-12 medium-12 small-12 columns total-align">
+
+				<div class="large-4 medium-4 small-12 columns left_column">
+					<div class="flex-video">
+					<iframe  src="https://www.youtube.com/embed/m89tB8n-RQg" width="400" height="225" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+
+				<div class="large-4 medium-4 small-12 columns center_column">
+					<div class="flex-video">
+					<iframe  src="https://www.youtube.com/embed/m89tB8n-RQg" width="400" height="225" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+
+				<div class="large-4 medium-4 small-12 columns right_column">
+					<div class="flex-video">
+					<iframe  src="https://www.youtube.com/embed/m89tB8n-RQg" width="400" height="225" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</div>
+
+			</div>
+			
+			<div class="large-12 medium-12 small-12 columns total-align">
+
+				<div id="twitter_feed" class="large-5 medium-8 columns large-centered medium-centered">
+					<a class="twitter-timeline" href="https://twitter.com/devscola"
+						data-widget-id="595887775318200320"
+						data-chrome= "transparent noborder"
+						width="1700"
+			  			height="500"
+						>Tweets by @devscola</a>
+				</div>
+
+			</div>
+
+				
+
+			
+
+			
+		</section>
+
 	
 	<?php while ( have_posts() ) : the_post(); ?>
 	

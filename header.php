a6abb91624339a6e554b52a8c28512e967cd4e70
ElDav1d <dvivoes@yahoo.es>
2015-07-10 12:17:11 +0200

    Adding header
    no pilla los estilos

--- header.php
+++ header.php
@@ -35,14 +35,58 @@
 
 <body <?php body_class(); ?>>
 
-<div>
+	<div class="fixed">
+		<nav class="top-bar" data-topbar role="navigation">
+	    <ul class="title-area">
+	        <li class="logo">
+	            <a href="#WTF"><p>DEVSCOLA</p></a>
+	            <?php wp_nav_menu( array( 
+	                       'theme_location' => 'primary',
+	                       'container' => false,
+	                       'menu_class' => 'menu'
+	                 ) ); ?>
+	        </li>
+	        <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a>
+	        </li>
+	    </ul>
 
-	<header>
-		
-		<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-			
-		<nav>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+	    	<section class="top-bar-section">
+		        <ul class="right">
+		            <li class="divider"></li>
+		            <li><a href="#WTF">DEVSCOLA WTF?</a></li>
+		            <li class="divider"></li>
+		            <li><a href="#TRACKS">TRACKS</a></li>
+		            <li class="divider"></li>
+		            <li><a href="#COMPROMISO">COMPROMISO</a></li>
+		            <li class="divider"></li>
+		            <li class="has-dropdown"><a>DEVSCOLA WHO?</a>
+		                <ul class="dropdown">
+			                	<li><a href="#WHO">DEVSCOLA WHO?</a></li>
+		                    <li class="divider"></li>
+		                    <li><a href="#WHO-PICS">INTEGRANTES</a></li>
+		                    <li class="divider"></li>
+		                    <li><a href="#MENTORING">MENTORING</a></li>
+		                    <li class="divider"></li>
+		                </ul>
+
+		            <li class="divider"></li>
+		            <li class="has-dropdown"><a>PARTICIPAR</a>
+		                <ul class="dropdown">
+			                	<li><a href="#JOIN">PARTICIPAR</a></li>
+		                    <li class="divider"></li>
+		                    <li><a href="#SPONSOR">APOYAR</a></li>
+		                    <li class="divider"></li>
+		                    <li><a href="#SOCIAL">SOCIAL</a></li>
+		                    <li class="divider"></li>
+		                </ul>
+		      
+		            <li class="divider"></li>
+		            <li class="active"><a href="#CONTACT">CONTACT</a></li>
+	 			</ul>
+ 			</section>
+
 		</nav>
+	</div>
 
-	</header>
+
+

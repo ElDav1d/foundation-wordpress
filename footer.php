159098101511b4fabe39206de686fc10a91da3d1
ElDav1d <dvivoes@yahoo.es>
2015-07-10 12:21:02 +0200

    Adding footer

--- footer.php
+++ footer.php
@@ -4,6 +4,30 @@
  */
 ?>
 
+<footer id="CONTACT">
+		<section class="row">
+
+			<div class="large-12 medium-12 small-12 columns total-align">
+
+				<div class="large-3 medium-4 small-9 columns right align-right">
+					<img class="foot_logo right small-right" src="assets/images/Logotipo-05.png" alt="Logo Geekshubs">
+				</div>
+
+				<div class="large-1 medium-1 small-2 columns right align-right small-left">
+				<img class="foot_logo right" src="assets/images/Logotipo-06.png" alt="Logo Devscola">
+				</div>
+
+				<div class="large-8 medium-7 small-12 columns left foot_text align-left">
+					<p>DEVSCOLA se gestiona y aloja en GEEKSHUBS</p>
+					<a href="https://goo.gl/maps/aBApT">Avd Catalunya nº11, Entresuelo</a>
+					<br>
+					<a id="MAIL" href="mailto:hola@devscola.org">hola@devscola.org</a>
+				</div>
+
+			</div>
+
+		</section>
+	</footer>
 <?php wp_footer(); ?>
 
 </body>

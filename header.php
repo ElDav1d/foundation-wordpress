841742994498e065ccd6663f331adee1df5d22a5
ElDav1d <dvivoes@yahoo.es>
2015-07-10 11:59:05 +0200

    Adding <head>

--- header.php
+++ header.php
@@ -2,11 +2,32 @@
 /**
  * The Header for our theme.
  */
+
+
 ?><!DOCTYPE html>
-<html>
+<html class="no-js" lang="es">
+
 <head>
 
+	
+	<meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />	
+	
+	<link rel="stylesheet" href="assets/stylesheets/foundation/foundation.css" />
+	<link rel="stylesheet" href="assets/stylesheets/foundation/normalize.css" />
+	<link rel="stylesheet" href="assets/stylesheets/style.css" />
+	<link rel="stylesheet" type="text/css" href="assets/stylesheets/web fonts/dejavusans_extralight_macroman/stylesheet.css" />
+	<link rel="stylesheet" type="text/css" href="assets/stylesheets/web fonts/dejavusans_book_macroman/stylesheet.css" />
+	<link rel="stylesheet" type="text/css" href="assets/stylesheets/web fonts/dejavusans_bold_macroman/stylesheet.css" />
+
+	<link rel="shortcut icon" href="assets/images/favicon.ico">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
+
+	<link rel="stylesheet" href="assets/owl/owl-carousel/owl.carousel.css">
+	<link rel="stylesheet" href="assets/owl/owl-carousel/owl.theme.css">
+
+
+	<script src="assets/scripts/vendor/modernizr.js"></script>
 	
 	<?php wp_head(); ?>
 	
